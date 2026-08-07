bbe9c05270e86c0e5eaa4d50db753c98a5075d16
termer <termer@termer.net>
2026-08-07 08:43:21 -0400

    live count demo

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -5,3 +5,32 @@ const host = window.location.hostname + ':5172'
 const hows = new Hows(`ws://${host}/compat/hows`)
 
 window.fetch = hows.fetch.bind(hows)
+
+const consoleElem = document.createElement('textarea')
+consoleElem.readOnly = true
+consoleElem.style.position = 'fixed'
+consoleElem.style.top = '0'
+consoleElem.style.left = '0'
+consoleElem.style.width = '100vw'
+consoleElem.style.height = '100vh'
+document.body.appendChild(consoleElem)
+
+function println(str: string) {
+	consoleElem.textContent += str + '\n'
+}
+
+const utf8Decoder = new TextDecoder('utf-8')
+
+println('starting')
+const countRes = await fetch('/count')
+const reader = countRes.body!.getReader()
+while (true) {
+	const { done, value } = await reader.read()
+	if (value) {
+		println(utf8Decoder.decode(value))
+	}
+	if (done) {
+		break
+	}
+}
+println('done')
